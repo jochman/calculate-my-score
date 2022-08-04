1be0e9925ab348ec7b3773b8f5a2835df5bcba3f
Bar Hochman <ilnest.bar@gmail.com>
2022-08-04 21:51:37 +0300

    ✨ new: calculate-my-score

---
+++ calculate_my_score/main.py
@@ -0,0 +1,22 @@
+import typer
+import rich
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    scores: list[str] = typer.Argument(
+        ...,
+        show_default=False,
+        help="Pairs of score in the format of SCORE:WEIGHT",
+    )
+):
+    """Calculate your score in a course."""
+    score_pairs = [map(float, score.split(":")) for score in scores]
+    scores_for_weight, total = 0.0, 0.0
+    for score, weight in score_pairs:
+        total += weight
+        scores_for_weight += score * weight
+    score_made = scores_for_weight / total
+    rich.print(f"Your score is {score_made:.2f}/{total:.0f}")
---
+++ tests/test_calculate_my_score.py
@@ -0,0 +1,14 @@
+from typer.testing import CliRunner
+from calculate_my_score.main import app
+import pytest
+
+runner = CliRunner()
+
+
+@pytest.mark.parametrize(
+    "scores, expected",
+    [(["80:80", "90:20"], "82.00/100"), (["100:100"], "100.00/100")],
+)
+def test_app(scores, expected):
+    result = runner.invoke(app, scores)
+    assert str(expected) in result.output
